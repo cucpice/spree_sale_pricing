37f4ab140737132bae8fe1c5ec8e51dc3fe4c59c
Putt <junrai82@gmail.com>
2015-12-25 16:08:18 +0700

    comment out admin deface that causes error

--- app/overrides/admin.rb
+++ app/overrides/admin.rb
@@ -1,42 +1,42 @@
-Deface::Override.new({
-  virtual_path: "spree/admin/variants/index",
-  name: "add_sale_prices_button_to_variants_index",
-  insert_before: "td.actions",
-  text: '<%= link_to_with_icon("usd", "", admin_variant_sale_prices_path(@variant), { no_text: true )} %>',
-  original: '856e87261a419d5b5bc0a734f2ec481c4c2fe0f7',
-  disabled: false
-})
+# Deface::Override.new({
+#   virtual_path: "spree/admin/variants/index",
+#   name: "add_sale_prices_button_to_variants_index",
+#   insert_before: "td.actions",
+#   text: '<%= link_to_with_icon("usd", "", admin_variant_sale_prices_path(@variant), { no_text: true )} %>',
+#   original: '856e87261a419d5b5bc0a734f2ec481c4c2fe0f7',
+#   disabled: false
+# })
 
-Deface::Override.new({
-  :virtual_path => "spree/admin/variants/index",
-  :name => "add_msrp_to_variant_index_header",
-  :replace_contents => "[data-hook='variants_header']",
-  :partial => "spree/admin/variants/variants_header"
-})
+# Deface::Override.new({
+#   :virtual_path => "spree/admin/variants/index",
+#   :name => "add_msrp_to_variant_index_header",
+#   :replace_contents => "[data-hook='variants_header']",
+#   :partial => "spree/admin/variants/variants_header"
+# })
+#
+# Deface::Override.new({
+#   :virtual_path => "spree/admin/variants/index",
+#   :name => "add_msrp_to_variant_index_row",
+#   :replace_contents => "[data-hook='variants_row']",
+#   :partial => "spree/admin/variants/variants_row"
+# })
 
-Deface::Override.new({
-  :virtual_path => "spree/admin/variants/index",
-  :name => "add_msrp_to_variant_index_row",
-  :replace_contents => "[data-hook='variants_row']",
-  :partial => "spree/admin/variants/variants_row"
-})
 
+# Deface::Override.new({
+#   virtual_path: "spree/admin/products/_form",
+#   name: "add_on_sale_to_product_form",
+#   insert_bottom: "[data-hook='admin_product_form_price']",
+#   text: '<% if @product.on_sale? %><strong><%= "(On sale for #{@product.display_price})" %></strong><% end %>',
+#   disabled: false
+# })
 
-Deface::Override.new({
-  virtual_path: "spree/admin/products/_form",
-  name: "add_on_sale_to_product_form",
-  insert_bottom: "[data-hook='admin_product_form_price']",
-  text: '<% if @product.on_sale? %><strong><%= "(On sale for #{@product.display_price})" %></strong><% end %>',
-  disabled: false
-})
-
-Deface::Override.new({
-  virtual_path: "spree/admin/variants/_form",
-  name: "add_on_sale_to_variant_form",
-  insert_bottom: "[data-hook='price']",
-  text: '<% if @variant.on_sale? %><strong><%= "(On sale for #{@variant.display_price})" %></strong><% end %>',
-  disabled: false
-})
+# Deface::Override.new({
+#   virtual_path: "spree/admin/variants/_form",
+#   name: "add_on_sale_to_variant_form",
+#   insert_bottom: "[data-hook='price']",
+#   text: '<% if @variant.on_sale? %><strong><%= "(On sale for #{@variant.display_price})" %></strong><% end %>',
+#   disabled: false
+# })
 
 Deface::Override.new({
   virtual_path: "spree/admin/products/new",
@@ -46,9 +46,9 @@ Deface::Override.new({
   disabled: false
 })
 
-Deface::Override.new({
-  :virtual_path => "spree/admin/variants/_form",
-  :name => "add_msrp_to_variant_form",
-  :insert_after => "[data-hook='sku']",
-  :partial => "spree/admin/variants/variant_msrp_field"
-})
+# Deface::Override.new({
+#   :virtual_path => "spree/admin/variants/_form",
+#   :name => "add_msrp_to_variant_form",
+#   :insert_after => "[data-hook='sku']",
+#   :partial => "spree/admin/variants/variant_msrp_field"
+# })
